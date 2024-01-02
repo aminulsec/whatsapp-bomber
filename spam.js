ded76bba822c10d1ece561472096ab011c2850b4
Aminul Islam <71552351+aminulsec@users.noreply.github.com>
2024-01-02 06:43:37 +0530

    Update spam.js

--- spam.js
+++ spam.js
@@ -2,7 +2,7 @@ var message = prompt("Enter your message", "‎");
 var counter = parseInt(prompt("How many Times ?", 10));
 window.InputEvent = window.Event || window.InputEvent;
 var event = new InputEvent("input", { bubbles: true });
-var textbox = document.getElementsByClassName("_13NKt copyable-text selectable-text")[1];
+var textbox = document.getElementsByClassName("selectable-text copyable-text iq0m558w g0rxnol2")[1];
 for (let index = 0; index < counter; index++) {
   textbox.innerHTML = message;
   textbox.dispatchEvent(event);
